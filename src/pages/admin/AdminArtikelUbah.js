8ccfd6b1afb12d6a31528e3ba5d0d0d4c586dbc1
Ahmad Mursyidan <ahmadmursyidan201@gmail.com>
2022-11-25 22:12:36 +0700

    Menambahkan halaman ubah artikel

---
+++ src/pages/admin/AdminArtikelUbah.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { getArtikel, updateArtikel } from '../../data/artikel-source';
+import SwalCustom from '../../data/swal-custom';
+import useInput from '../../hooks/useInput';
+import renderArtikelFormTemplate from '../../utils/template-creator';
+import AdminTemplate from './AdminTemplate';
+
+function AdminArtikelUbah() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [judul, onJudulChange, setJudul] = useInput('');
+  const [isi, onIsiChange, setIsi] = useInput('');
+  const [penulis, onPenulisChange, setPenulis] = useInput('');
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    SwalCustom.showLoading();
+    if (await updateArtikel({
+      id,
+      data: {
+        judul,
+        isi,
+        penulis,
+      },
+    })) {
+      await SwalCustom.showSuccess('Berhasil mengubah artikel');
+      navigate('/artikel');
+    }
+  }
+
+  React.useEffect(() => {
+    (async () => {
+      const result = await getArtikel(id);
+
+      if (result) {
+        setJudul(result.data.judul);
+        setIsi(result.data.isi);
+        setPenulis(result.data.penulis);
+      }
+    })();
+  }, []);
+
+  function renderContent() {
+    return (
+      <>
+        <h2 className="fs-4 mb-4">Ubah Artikel</h2>
+
+        {renderArtikelFormTemplate(
+          'Ubah Artikel',
+          {
+            onSubmit,
+            judul,
+            onJudulChange,
+            isi,
+            onIsiChange,
+            penulis,
+            onPenulisChange,
+          },
+        )}
+      </>
+    );
+  }
+
+  return <AdminTemplate content={renderContent()} />;
+}
+
+export default AdminArtikelUbah;
