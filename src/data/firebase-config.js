1a0c4c72ad5b95471e269a325c0ad5799d4f56b8
Ahmad Mursyidan <64141200+twensz@users.noreply.github.com>
2022-12-08 06:36:30 +0700

    Menghapus konfigurasi yang tidak digunakan

--- src/data/firebase-config.js
+++ src/data/firebase-config.js
@@ -10,15 +10,6 @@ const firebaseConfig = {
   messagingSenderId: '455109609482',
   appId: '1:455109609482:web:c53def8c0f20cca6ef37f3',
 };
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDIs8QmzQxSQOrN_NamPgjU5cM5MD5yWH0',
-//   authDomain: 'test-firebase-edfa9.firebaseapp.com',
-//   projectId: 'test-firebase-edfa9',
-//   storageBucket: 'test-firebase-edfa9.appspot.com',
-//   messagingSenderId: '596854476641',
-//   appId: '1:596854476641:web:987ce15ecd0a86e81abf20',
-//   measurementId: 'G-FHLW71NCKN',
-// };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
